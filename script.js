edcf9325f9f368d3e9dd6b45bd23568cde8f5709
Rahul Mishra <rahulmishra102000@gmail.com>
2020-10-22 15:55:30 +0530

    JavaScript file compeletd

---
+++ script.js
@@ -0,0 +1,39 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = '1 Jan 2021';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    // To see the output in the console.
+    // console.log(days, hours, mins, seconds);
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+// Adding zero if only single digit is present in time
+function formatTime(time) {
+    // Uisng terniary operator
+    return time < 10 ? `0${time}` : time;
+}
+
+// Initial Call
+countdown();
+
+
+// Here, we had seted an interval which will call the countdown evey second.
+setInterval(countdown, 1000);
